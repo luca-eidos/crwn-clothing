00012a990582c61963a2969a8e2e99f8ff28c69f
Luca <luca.giovannini@eidos-tech.com>
2021-04-21 12:44:09 +0200

    updating our app with a shop page, collection preview and collection item components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
 import React from 'react';
-import {Switch, Route, Link} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
-
-const HatsPage = (props) => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
-
-const TopicsPage = (props) => (
-  <div>
-    <h1>TOPIC LIST PAGE</h1>
-    <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link> 
-    <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link> 
-    <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
-  </div>
-);
-
-const TopicIdPage = (props) => (
-  <div>
-    <h1>TOPIC {props.match.params.TopicId} PAGE</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}/>
-        <Route path="/hats" component={HatsPage}/>
-        <Route exact path="/topics" component={TopicsPage}/>
-        <Route exact path="/topics/:TopicId" component={TopicIdPage}/>
+        <Route exact path="/shop" component={ShopPage}/>
       </Switch>
     </div>
   );
