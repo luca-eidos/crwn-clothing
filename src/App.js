517304760fe7fbe58c31914fb61f66a176a42267
Luca <luca.giovannini@eidos-tech.com>
2021-05-05 14:45:17 +0200

    updates our application App component, sign-in component and sign-up component to use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -11,42 +11,33 @@ import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/signin-signup/signin-signup.component'
 import CheckoutPage from './pages/checkout/checkout.component' 
 
-class App extends React.Component {
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { checkUserSession } = this.props;
-
+const App = ({currentUser, checkUserSession}) => {
+  useEffect(() => {
     checkUserSession();
-  }
+  }, [checkUserSession])
 
-  componentWillUnmount() {
-    // this.unsubscribeFromAuth();
-  }
+  return (
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route path="/shop" component={ShopPage}/>
+        <Route exact path="/checkout" component={CheckoutPage}/>
+        <Route 
+          exact 
+          path="/signin" 
+          render={() =>  
+            currentUser? (
+              <Redirect to="/"/>
+            ) : ( 
+              <SignInAndSignUpPage/>
+            )
+          }
+        />
+      </Switch>
+    </div>
+  );
 
-  render() {
-    return (
-      <div>
-        <Header/>
-        <Switch>
-          <Route exact path="/" component={HomePage}/>
-          <Route path="/shop" component={ShopPage}/>
-          <Route exact path="/checkout" component={CheckoutPage}/>
-          <Route 
-            exact 
-            path="/signin" 
-            render={() =>  
-              this.props.currentUser? (
-                <Redirect to="/"/>
-              ) : ( 
-                <SignInAndSignUpPage/>
-              )
-            }
-          />
-        </Switch>
-      </div>
-    );
-  }
 }
 
 const mapStateToProps = createStructuredSelector({
